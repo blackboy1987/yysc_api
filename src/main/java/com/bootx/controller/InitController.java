fc84a4bcd9b8ad9ced68c0edd29a64ad38734d2d
1169794338@qq.com <1169794338@qq.com>
2023-12-02 12:50:32 +0800

    热门搜索

--- src/main/java/com/bootx/controller/InitController.java
+++ src/main/java/com/bootx/controller/InitController.java
@@ -14,12 +14,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author black
@@ -90,6 +88,7 @@ public class InitController {
         for (Category category : categories) {
             String url = "https://m.shouji.com.cn"+category.getUrl();
             for (int i = 1; i <= 1000; i++) {
+                long start = System.currentTimeMillis();
                 String url1 = url.replace("_1", "_"+i);
                 try {
                     Document parse = Jsoup.parse(new URL(url1).openStream(), "utf-8", url);
@@ -120,65 +119,23 @@ public class InitController {
                         soft.setWeekDownloadsDate(new Date());
                         soft.setMonthDownloadsDate(new Date());
                         soft.setScore(new Random().nextDouble(10));
-
-
-
-
+                        softService.save(soft);
                         try {
-                            softDetail(soft);
+                            detail(soft);
                         } catch (IOException e) {
-                            e.printStackTrace();
+                            throw new RuntimeException(e);
                         }
-                        Soft finalSoft = soft;
-                        new Thread(()->{
-                            if(finalSoft.isNew()){
-                                softService.save(finalSoft);
-                            }else{
-                                softService.update(finalSoft);
-                            }
-                        }).start();
                     });
-
-
-
+                    System.out.println("end："+(System.currentTimeMillis()-start));
                     if(li.size()<20){
                         break;
                     }
-                }catch (Exception e){
+                }catch (Exception ignored){
 
                 }
             }
         }
         return Result.success();
-    }
-
-    public void softDetail(Soft soft) throws IOException {
-        String url = "https://m.shouji.com.cn"+soft.getUrl();
-        Document parse = Jsoup.parse(new URL(url).openStream(), "utf-8", url);
-        Element infocont = parse.getElementById("infocont");
-        Elements h1 = infocont.getElementsByTag("h1");
-        String fullName = h1.first().text();
-        soft.setFullName(fullName);
-        Elements div = infocont.getElementsByTag("div");
-        Element element = div.get(1);
-        String updateDate = element.text().replace("更新","");
-        soft.setUpdateDate(updateDate);
-        Element element1 = div.get(2);
-        String memo = element1.text();
-        soft.setMemo(memo);
-        Element element2 = div.get(3);
-        String downloadUrl = element2.getElementsByTag("a").first().attr("href");
-        soft.setDownloadUrl(downloadUrl);
-        Element imgItem = parse.getElementsByClass("img_item").first();
-        Elements img = imgItem.getElementsByTag("img");
-        List<String> imgs = new ArrayList<>();
-        img.forEach(i->{
-            imgs.add(i.attr("src"));
-        });
-       // soft.setImages(StringUtils.join(imgs,","));
-        Element txtcont = parse.getElementsByClass("txtcont").first();
-        String description = txtcont.html();
-        //soft.setDescription(description);
     }
 
 
@@ -309,5 +266,82 @@ public class InitController {
         }
 
         return Result.success();
+    }
+
+    public void detail(Soft soft) throws IOException {
+        if(soft==null){
+            return ;
+        }
+        Document parse = null;
+        String url = "https://www.shouji.com.cn"+soft.getUrl();
+        try {
+            parse = Jsoup.parse(new URL(url).openStream(), "utf-8", url);
+        }catch (Exception e){
+            return;
+        }
+        Document finalParse = parse;
+        executorService.execute(()->{
+            // 基本信息
+            Elements info_cent = finalParse.getElementsByClass("info_cent");
+            SoftExt softExt = new SoftExt();
+            softExt.setSoft(soft);
+            if(!info_cent.isEmpty()){
+                Element first = info_cent.first();
+                Elements span = first.getElementsByTag("span");
+                Elements p = first.getElementsByTag("p");
+                for (int i = 0; i < span.size(); i++) {
+                    String element = span.get(i).text();
+                    String text = p.get(i).text();
+                    if(StringUtils.equals("版本：",element.trim())){
+                        soft.setVersionName(text.trim());
+                    }else if(StringUtils.equals("大小：",element.trim())){
+                        soft.setSize(text.trim());
+                    }else if(StringUtils.equals("更新：",element.trim())){
+                        soft.setUpdateDate(text.trim());
+                    }else if(StringUtils.equals("资费：",element.trim())){
+                        if(StringUtils.contains("免费",text)){
+                            softExt.setPaidType(0);
+                        }else {
+                            softExt.setPaidType(1);
+                        }
+
+                    }else if(StringUtils.equals("广告：",element.trim())){
+                        if(StringUtils.contains("没有",text)){
+                            softExt.setAdType(0);
+                        }else {
+                            softExt.setAdType(1);
+                        }
+                    }
+                }
+
+
+            }
+            Elements processingbar = finalParse.getElementsByClass("processingbar");
+            if (!processingbar.isEmpty()){
+                Elements font = processingbar.first().getElementsByTag("font");
+                if(!font.isEmpty()){
+                    String score = font.text();
+                    soft.setScore(Double.valueOf(score));
+                }
+            }
+
+            // 评论
+            Elements lef1 = finalParse.getElementsByClass("Lef1_cent");
+            String html = lef1.html();
+            softInfoService.create(soft,html);
+
+            // 图片
+            Elements snapShotCont = finalParse.getElementsByClass("snapShotCont");
+            List<String> images = new ArrayList<>();
+            if(snapShotCont!=null){
+                Elements img = snapShotCont.first().getElementsByTag("img");
+                img.forEach(im->{
+                    images.add(im.attr("src"));
+                });
+            }
+            softImageService.create(soft,images);
+            softService.update(soft);
+            softExtService.create(softExt);
+        });
     }
 }
